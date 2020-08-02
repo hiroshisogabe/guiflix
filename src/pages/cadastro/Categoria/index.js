4843fb3ec10e49fea63e0826ac1003e5ec2d5b9f
Guilherme H. S <sogabe.guilherme@gmail.com>
2020-08-02 14:28:52 -0300

    Criando componentes de campos de formulário e utilização de lib para slide de carousel

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,23 +1,79 @@
-import React from 'react';
-import PaginaDefault from '../../../components/PageDefault';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import FormField from '../../../components/FormField';
+import PaginaDefault from '../../../components/PageDefault';
 
 function CadastroCategoria() {
+    const valoresIniciais = {
+        nome: '',
+        descricao: '',
+        cor: '',
+    }
+    
+    const [categorias, setCategorias] = useState([]);
+    const [values, setValues] = useState(valoresIniciais);
+    
+    function setValue(chave, valor) {
+        setValues({
+            ...values,
+            [chave]: valor,
+        });
+    }
+
+    function handleChange(evento) {
+        setValue(
+            evento.target.getAttribute('name'), 
+            evento.target.value
+        );
+    }
+    
     return (
         <>
             <PaginaDefault>
-                <h1>Cadastro de Categoria</h1>
-                <form>
-                    <label>
-                        Nome da Categoria:
-                        <input
+                <h1>Cadastro de Categoria: {values.nome}</h1>
+                <form onSubmit={function handleSubmit(evento) {
+                    evento.preventDefault();
+                    setCategorias([
+                        ...categorias,
+                        values
+                    ]);
+                    setValues(valoresIniciais);
+                }}>
+                    <FormField 
+                        label="Nome da Categoria"
                         type="text"
-                        />
-                    </label>
+                        name="nome"
+                        value={values.nome}
+                        onChange={handleChange}
+                    />
+                    <FormField
+                        label="Descrição"
+                        type="text"
+                        name="descricao"
+                        value={values.descricao}
+                        onChange={handleChange}
+                    />
+                    <FormField
+                        label="Cor"
+                        type="color"
+                        name="cor"
+                        value={values.cor}
+                        onChange={handleChange}
+                    />
+                    
                     <button>
                         Cadastrar
                     </button>
                 </form>
+                <ul>
+                    {categorias.map((categoria, indice) => {
+                        return (
+                            <li key={`${categoria}${indice}`}>
+                                {categoria.nome}
+                            </li>
+                        )
+                    })}
+                </ul>
                 <Link to="/">
                     Ir para home
                 </Link>
